8917f227a4be0c8430551a3cca26836cfc65b5a1
V T R Ravi Kumar <vtrravikumar@gmail.com>
2026-09-17 13:17:31 +0530

    Expose digitization preprocessors

--- digitization/__init__.py
+++ digitization/__init__.py
@@ -1,5 +1,14 @@
 """Source-document digitization utilities for VTR Press."""
 
 from .ocr import OCRConfig, OCR_PROFILES, TesseractOCR, get_ocr_profile
+from .preprocess import PassthroughPreprocessor, PillowPreprocessor, PreprocessConfig
 
-__all__ = ["OCRConfig", "OCR_PROFILES", "TesseractOCR", "get_ocr_profile"]
+__all__ = [
+    "OCRConfig",
+    "OCR_PROFILES",
+    "TesseractOCR",
+    "get_ocr_profile",
+    "PassthroughPreprocessor",
+    "PillowPreprocessor",
+    "PreprocessConfig",
+]
