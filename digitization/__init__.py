c7a4be2779576d7f1a42e5faf36f49f177391502
V T R Ravi Kumar <vtrravikumar@gmail.com>
2026-09-17 13:50:09 +0530

    Export digitization source asset helper

--- digitization/__init__.py
+++ digitization/__init__.py
@@ -1,5 +1,6 @@
 """Source-document digitization utilities for VTR Press."""
 
+from .assets import preserve_page_image
 from .ocr import OCRConfig, OCR_PROFILES, TesseractOCR, get_ocr_profile
 from .preprocess import PassthroughPreprocessor, PillowPreprocessor, PreprocessConfig
 from .structure import PageStructure, StructureClassification, classify_structure
@@ -15,4 +16,5 @@ __all__ = [
     "PageStructure",
     "StructureClassification",
     "classify_structure",
+    "preserve_page_image",
 ]
