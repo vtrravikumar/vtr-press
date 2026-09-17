31ef44505107e1cb0536a5dfe6f671a622e512c9
V T R Ravi Kumar <vtrravikumar@gmail.com>
2026-09-17 16:54:29 +0530

    Export table grid layout types

--- digitization/__init__.py
+++ digitization/__init__.py
@@ -1,7 +1,7 @@
 """Source-document digitization utilities for VTR Press."""
 
 from .assets import preserve_page_image
-from .layout import VisualAnalysis, analyze_page
+from .layout import TableGrid, VisualAnalysis, VisualRegion, analyze_page
 from .ocr import OCRConfig, OCR_PROFILES, TesseractOCR, get_ocr_profile
 from .preprocess import PassthroughPreprocessor, PillowPreprocessor, PreprocessConfig
 from .review import build_review_markers
@@ -18,6 +18,8 @@ __all__ = [
     "PageStructure",
     "StructureClassification",
     "classify_structure",
+    "VisualRegion",
+    "TableGrid",
     "VisualAnalysis",
     "analyze_page",
     "preserve_page_image",
